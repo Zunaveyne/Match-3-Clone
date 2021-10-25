17c938d9e4770b379cf7a8867ea777fa88ae58ea
Александр Щелочков <shchelochkov.alex@gmail.com>
2021-10-25 13:05:15 +0300

    Добавьте файлы проекта.

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,217 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState //Limit player movement using a state machine
+{
+    wait,
+    move
+}
+
+public class Board : MonoBehaviour
+{
+    public GameState currentState = GameState.move;
+    public int width;
+    public int heigth;
+    public int offSet; // Высота падения
+    public GameObject tilePrefab; // Префаб ячейки
+    private BackgroundTile[,] allTiles; // Все ячейки
+    public GameObject destroyEffect;
+    public GameObject[] dots; // Префаб из всех точек
+    public GameObject[,] allDots; // Все точки
+    private FindMatches findMatches;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        findMatches = FindObjectOfType<FindMatches>();
+        allTiles = new BackgroundTile[width, heigth];
+        allDots = new GameObject[width, heigth];
+        SetUp();
+    }
+
+    // Update is called once per frame
+    private void SetUp()
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < heigth; j++)
+            {
+                Vector2 tempPosition = new Vector2(i, j + offSet);
+                GameObject backgroundTile = Instantiate(tilePrefab, tempPosition, Quaternion.identity) as GameObject;
+                backgroundTile.transform.parent = this.transform; // Привязываем генерируемые ячейки к доске
+                backgroundTile.name = "(" + i + "," + j + ")"; // Название ячейки адрес
+                int dotToUse = Random.Range(0, dots.Length); // Выбираем рандомную из массива
+
+                int maxIterations = 0; // Защита от бесконечности цикла
+                while(MatchesAt(i, j, dots[dotToUse]) && maxIterations < 100) // Проверяем есть ли совпадения и меняем объект пока их не будет  
+                {
+                    dotToUse = Random.Range(0, dots.Length);
+                    maxIterations++;
+                    //Debug.Log(maxIterations);
+                }
+                maxIterations = 0;
+
+                GameObject dot = Instantiate(dots[dotToUse], tempPosition, Quaternion.identity); // Создаем ее
+                dot.GetComponent<Dot>().row = j;
+                dot.GetComponent<Dot>().column = i;
+                dot.transform.parent = this.transform; // Прицепляем к ячейке
+                dot.name = dots[dotToUse].name + "(" + i + "," + j + ")"; // Имя как у ячейки
+                allDots[i, j] = dot;
+            }
+        }
+    }
+
+    private bool MatchesAt( int column, int row, GameObject piece)
+    {
+        if (column > 1)          // Проверяем совпадение по строке 
+        {
+            // Проверяем до 2 объектов слева
+            if (allDots[column - 1, row].tag == piece.tag && allDots[column - 2, row].tag == piece.tag)
+            {
+                return true;
+            }
+        }
+
+        if (row > 1)        // Проверяем совпадение по ряду
+        {
+            // Проверяем до 2 объектов внизу
+            if (allDots[column, row - 1].tag == piece.tag && allDots[column, row - 2].tag == piece.tag)
+            {
+                return true;
+            }
+        }
+        /* Изначальный код
+        if (column > 1 && row > 1)
+        {
+            // Проверяем до 2 объектов слева
+            if (allDots[column - 1, row].tag == piece.tag && allDots[column - 2, row].tag == piece.tag)
+            {
+                return true;
+            }
+            // Проверяем до 2 объектов внизу
+            if (allDots[column, row - 1].tag == piece.tag && allDots[column, row - 2].tag == piece.tag)
+            {
+                return true;
+            }
+        }
+        else if (column <= 1 || row <= 1)
+        {
+            if (row > 1)
+            {
+                if (allDots[column, row - 1].tag == piece.tag && allDots[column, row - 2].tag == piece.tag)
+                {
+                    return true;
+                }
+            }
+            if (column > 1)
+            {
+                if (allDots[column - 1, row].tag == piece.tag && allDots[column - 2, row].tag == piece.tag)
+                {
+                    return true;
+                }
+            }
+        }
+        */
+        return false;
+    }
+
+    private void DestroyMatchesAt(int column, int row) // Разрушаем объект если его переменная isMatched истина
+    {
+        if (allDots[column, row].GetComponent<Dot>().isMatched)
+        {
+            findMatches.currentMatches.Remove(allDots[column, row]);
+            Instantiate(destroyEffect, allDots[column, row].transform.position, Quaternion.identity);
+            //GameObject particle = Instantiate(destroyEffect, allDots[column, row].transform.position, Quaternion.identity);
+            //Destroy(particle, .5f);
+            Destroy(allDots[column, row]);
+            allDots[column, row] = null;
+        }
+    }
+
+    public void DestroyMatches() // Проверяем все поле на совпадения
+    {
+        for(int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < heigth; j++)
+            {
+                if (allDots[i, j] != null) DestroyMatchesAt(i, j);
+            }
+        }
+        StartCoroutine(DecreaseRowCo());
+    }
+
+    private IEnumerator DecreaseRowCo() // Удаление рядов
+    {
+        int nullCount = 0; // Кол-во пустых ячеек
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < heigth; j++)
+            {
+                if (allDots[i, j] == null)
+                {
+                    nullCount++; //
+                }
+                else if (nullCount > 0)
+                {
+                    allDots[i, j].GetComponent<Dot>().row -= nullCount;
+                    allDots[i, j] = null;
+                }
+            }
+            nullCount = 0;
+        }
+        yield return new WaitForSeconds(.4f);
+        StartCoroutine(FillBoardCo());
+    }
+
+    private void RefilBoard() //Перезаполнение доски
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < heigth; j++)
+            {
+                if (allDots[i, j] == null)
+                {
+                    Vector2 tempPosition = new Vector2(i, j + offSet); //Координаты нулевой позиции
+                    int dotToUse = Random.Range(0, dots.Length);
+                    GameObject piece = Instantiate(dots[dotToUse], tempPosition, Quaternion.identity);
+                    allDots[i, j] = piece; //Создаем объект в нулевой позиции
+                    piece.GetComponent<Dot>().row = j;
+                    piece.GetComponent<Dot>().column = i;
+                }
+            }
+        }
+    }
+    
+    private bool MatchesOnBoard() //Проверка на совпадения в реальном врени
+    {
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < heigth; j++)
+            {
+                if (allDots[i , j] != null)
+                {
+                    if (allDots[i, j].GetComponent<Dot>().isMatched)
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    private IEnumerator FillBoardCo() //Перезаполнение доски и проверка на совпадения в реальном времени
+    {
+        RefilBoard();
+        yield return new WaitForSeconds(.5f);
+
+        while(MatchesOnBoard())
+        {
+            yield return new WaitForSeconds(.5f);
+            DestroyMatches();
+        }
+        yield return new WaitForSeconds(.5f);
+        currentState = GameState.move;
+    }
+}
---
+++ Assets/Scripts/Dot.cs
@@ -0,0 +1,211 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dot : MonoBehaviour
+{
+    [Header("Board Variables")]
+    public int column;
+    public int row;
+    public int previousColumn;
+    public int previousRow;
+    public int targetX;
+    public int targetY;
+    public bool isMatched = false; // Совпадение
+
+    private FindMatches findMatches;
+    private Board board;
+    private GameObject otherDot;
+    private Vector2 firstTouchPosition; // Координаты первой позиции
+    private Vector2 finalTouchPosition; //Координаты второй позиции
+    private Vector2 tempPosition; //Целевая позиция
+    public float swipeAngle = 0; // Угол взаимодействия
+    public float swipeResist = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        board = FindObjectOfType<Board>();
+        findMatches = FindObjectOfType<FindMatches>();
+        //targetX = (int)transform.position.x;
+        //targetY = (int)transform.position.y;
+        //row = targetY;
+        //column = targetX;
+        //previousColumn = column;
+        //previousRow = row;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // FindMatches();
+        if (isMatched)
+        {
+            SpriteRenderer mySprite = GetComponent<SpriteRenderer>();
+            mySprite.color = new Color(1f, 1f, 1f, .2f); // При совпадении меняем компонент
+        }
+        targetX = column;
+        targetY = row;
+        if (Mathf.Abs(targetX - transform.position.x) > .1) // Move towards the target
+        {
+            tempPosition = new Vector2(targetX, transform.position.y);
+            transform.position = Vector2.Lerp(transform.position, tempPosition, .4f);
+            if (board.allDots[column, row] != this.gameObject)
+            {
+                board.allDots[column, row] = this.gameObject;
+            }
+            findMatches.FindAllMatches();
+        }
+        else // Directly set the position
+        {   
+            tempPosition = new Vector2(targetX, transform.position.y);
+            transform.position = tempPosition;
+        }
+        if (Mathf.Abs(targetY - transform.position.y) > .1) // Move towards the target
+        {
+            tempPosition = new Vector2(transform.position.x, targetY);
+            transform.position = Vector2.Lerp(transform.position, tempPosition, .4f);
+            if (board.allDots[column, row] != this.gameObject)
+            {
+                board.allDots[column, row] = this.gameObject;
+            }
+            findMatches.FindAllMatches();
+        }
+        else // Directly set the position
+        {
+            tempPosition = new Vector2(transform.position.x, targetY);
+            transform.position = tempPosition;
+        }
+    }
+
+    public IEnumerator CheckMoveCo()
+    {
+        yield return new WaitForSeconds(.5f);
+        if (otherDot != null)
+        {
+            if (!isMatched && !otherDot.GetComponent<Dot>().isMatched)
+            {
+                otherDot.GetComponent<Dot>().row = row;
+                otherDot.GetComponent<Dot>().column = column;
+                row = previousRow;
+                column = previousColumn;
+                yield return new WaitForSeconds(.5f);
+                board.currentState = GameState.move;
+            }
+            else
+            {
+                board.DestroyMatches();
+            }
+            otherDot = null;
+        }
+        else board.currentState = GameState.move;
+    }
+
+    private void OnMouseDown()
+    {
+        if (board.currentState == GameState.move)
+        {
+            firstTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition); // Конвертирует в общую систему координат из кординаты объекта
+            // Debug.Log(firstTouchPosition);
+        }
+
+    }
+
+    private void OnMouseUp()
+    {
+        if (board.currentState == GameState.move)
+        { 
+            finalTouchPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            CalculateAngle();
+        }
+
+    }
+
+    void CalculateAngle()
+    {
+        if (Mathf.Abs(finalTouchPosition.y - firstTouchPosition.y) > swipeResist || Mathf.Abs(finalTouchPosition.x - firstTouchPosition.x) > swipeResist) // Проверка на то что две точки не совпадают
+        {
+            swipeAngle = Mathf.Atan2(finalTouchPosition.y - firstTouchPosition.y, finalTouchPosition.x - firstTouchPosition.x) * 180 / Mathf.PI; // Угол между касанием и отпусканием в радианах с переводом в градусы
+            //Debug.Log(swipeAngle);
+            MovePieces();
+            board.currentState = GameState.wait; //После действия со смещением режим ожидания 
+        }
+        else 
+        {
+            board.currentState = GameState.move;
+        }
+    }
+
+    void MovePieces()
+    {
+        if (swipeAngle > -45 && swipeAngle <= 45 && column < board.width - 1) // Правый свайп
+        {
+            otherDot = board.allDots[column + 1, row];
+            previousColumn = column;
+            previousRow = row;
+            otherDot.GetComponent<Dot>().column -= 1;
+            column += 1;
+            Debug.Log("Right");
+        }
+        else if (swipeAngle > 45 && swipeAngle <= 135 && row < board.heigth - 1) // Верхний свайп
+        { 
+            otherDot = board.allDots[column, row + 1];
+            previousColumn = column;
+            previousRow = row;
+            otherDot.GetComponent<Dot>().row -= 1;
+            row += 1;
+            Debug.Log("Up");
+        }
+        else if ((swipeAngle > 135 || swipeAngle <= -135) && column > 0 ) // Левый свайп
+        {
+            otherDot = board.allDots[column - 1, row];
+            previousColumn = column;
+            previousRow = row;
+            otherDot.GetComponent<Dot>().column += 1;
+            column -= 1;
+            Debug.Log("Left");
+        }
+        else if (swipeAngle < -45 && swipeAngle >= -135 && row > 0) // Нижний свайп
+        {
+            otherDot = board.allDots[column, row - 1];
+            previousColumn = column;
+            previousRow = row;
+            otherDot.GetComponent<Dot>().row += 1;
+            row -= 1;
+            Debug.Log("Down");
+        }
+        StartCoroutine(CheckMoveCo());
+    }
+
+    void FindMatches() // Поиск совпадений
+    {
+        if (column > 0 && column < board.width - 1) // По горизонтали
+        {
+            GameObject leftDot1 = board.allDots[column - 1, row]; // Левый объект
+            GameObject rightDot1 = board.allDots[column + 1, row]; // Правый объект
+            if (leftDot1 != null && rightDot1 != null)
+            {
+                if (leftDot1.tag == this.gameObject.tag && rightDot1.tag == this.gameObject.tag)
+                {
+                    leftDot1.GetComponent<Dot>().isMatched = true;
+                    rightDot1.GetComponent<Dot>().isMatched = true;
+                    isMatched = true; // Совпадение
+                }
+            }
+        }
+        if (row > 0 && row < board.heigth - 1) // По вертикали
+        {
+            GameObject upDot1 = board.allDots[column, row + 1]; // Верхний объект
+            GameObject downDot1 = board.allDots[column, row - 1]; // Нижний объект
+            if (upDot1 != null && downDot1 != null)
+            {
+                if (upDot1.tag == this.gameObject.tag && downDot1.tag == this.gameObject.tag)
+                {
+                    upDot1.GetComponent<Dot>().isMatched = true;
+                    downDot1.GetComponent<Dot>().isMatched = true;
+                    isMatched = true; // Совпадение
+                }
+            }
+        }
+    }
+}
